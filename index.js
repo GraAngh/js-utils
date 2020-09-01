f705b0dced79228ba49b89b73d7ccdb8c7d7f814
Gra Angh <graangh@gmail.com>
2020-09-01 08:54:02 +0300

    removing excess 'require' from [index.js]

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict';
 
 exports.Type            = require('.\\source\\Type.js');
-exports.Type_v2         = require('.\\source\\Type_v2.js');
 exports.PropertyHandler = require('.\\source\\PropertyHandler.js');
